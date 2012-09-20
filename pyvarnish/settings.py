fbc1a1ce1e41163cf4b27ed7a8b1cb2ae020fde2
James Pearson <james@ifixit.com>
2012-09-19 17:50:28 -0700

    Optionally pick up config values from ini file

--- pyvarnish/settings.py
+++ pyvarnish/settings.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
+import os
+try:
+    from ConfigParser import SafeConfigParser, NoOptionError, NoSectionError
+except ImportError:
+    # Renamed in py3k
+    from configparser import SafeConfigParser, NoOptionError, NoSectionError
+
 __author__ = 'John Moylan'
 
-VARNISH_SERVERS = ('server1', "server2",)
-SSH_CONFIG = "/home/username/.ssh/config"
-DEBUG = True
-CARBON_SERVER = 'localhost'
-CARBON_PORT = 2003
+defaults = {
+    'varnish_servers': 'server1, server2',
+    'ssh_config':  "/home/username/.ssh/config",
+    'debug':  'True',
+    'carbon_server':  'localhost',
+    'carbon_port':  '2003',
+}
 
+config = SafeConfigParser(defaults)
+# Loosely follow the XDG Base Directory Specification:
+# http://standards.freedesktop.org/basedir-spec/basedir-spec-0.6.html
+configPath = os.path.expanduser('~/.config/pyvarnish/config.ini')
+config.read(configPath)
   
+VARNISH_SERVERS = config.get('DEFAULT', 'varnish_servers').split(',')
+VARNISH_SERVERS = [server.strip() for server in VARNISH_SERVERS]
+SSH_CONFIG = config.get('DEFAULT', 'ssh_config')
+DEBUG = config.getboolean('DEFAULT', 'debug')
+CARBON_SERVER = config.get('DEFAULT', 'carbon_server')
+CARBON_PORT = config.getint('DEFAULT', 'carbon_port')
+
